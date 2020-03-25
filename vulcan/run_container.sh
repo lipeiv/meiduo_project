00fc56d180318cb6847abb4892a94d0569d5b9e9
lipeiv <lipeiv@qq.com>
2020-03-25 10:52:52 +0900

    'del'

--- vulcan/run_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if test $# -eq 0 ; then
-  docker run --rm -v /var/log:/opt/logs -e LOG_LEVEL=info valuepush/vulcan node pushResultChecker.js
-  docker run --rm -v /var/log:/opt/logs -e LOG_LEVEL=info valuepush/vulcan node pushSender.js
-else
-  docker run --rm -v /var/log:/opt/logs -e LOG_LEVEL=$1 valuepush/vulcan node pushResultChecker.js
-  docker run --rm -v /var/log:/opt/logs -e LOG_LEVEL=$1 valuepush/vulcan node pushSender.js
-fi
